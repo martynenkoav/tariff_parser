3ab1604cbe2d4ed806f3f78d40f7a9159a6c620f
Nastya <martynenkoav.18@mail.ru>
2023-03-26 23:19:01 +0300

    Added comments

--- src/main/java/TariffReports.java
+++ src/main/java/TariffReports.java
@@ -7,7 +7,7 @@ import java.util.HashMap;
 */
 
 
-public class Tariff_reports {
+public class TariffReports {
     public static void main(String[] args) throws IOException, ParseException {
         HashMap<String, UserCDR> map = Parser.parse("src/main/resources/cdr.txt");
         map.forEach((k, v) -> v.count_tariff());
